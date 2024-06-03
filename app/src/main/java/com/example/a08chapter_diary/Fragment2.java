9ef647da2636dcecca1fd64f2dbe814cbb7dc910
yyyyyyyyyoon <dbstj253@jbnu.ac.kr>
2024-06-03 23:41:34 +0900

    fragment 위치, 아이콘 변경

--- app/src/main/java/com/example/a08chapter_diary/Fragment2.java
+++ app/src/main/java/com/example/a08chapter_diary/Fragment2.java
@@ -1,9 +1,5 @@
 package com.example.a08chapter_diary;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.media.Image;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -11,26 +7,17 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import kotlinx.coroutines.flow.Flow;
 
 public class Fragment2 extends Fragment {
     private FlowerViewModel flowerViewModel;
-    private Adapter adapter =null;
-    private GridView gridView = null;
-
-
+    private ImageView imageView;
+    private TextView textView;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,85 +28,33 @@ public class Fragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_2, container, false);
+        imageView = view.findViewById(R.id.imageView);
+        textView = view.findViewById(R.id.textView);
 
-        gridView = view.findViewById(R.id.gridView1);
-        adapter = new Adapter(getActivity());
-        gridView.setAdapter(adapter);
-
-        // LiveData를 관찰하여 데이터가 변경될 때마다 그리드뷰 갱신
-        flowerViewModel.getFlowerImages().observe(getViewLifecycleOwner(), new Observer<List<Integer>>() {
+        flowerViewModel.getSaveCount().observe(getViewLifecycleOwner(), new Observer<Integer>() {
             @Override
-            public void onChanged(List<Integer> Images) {
-                if (Images != null) {
-                    adapter.setFlowerImages(Images);
+            public void onChanged(Integer count) {
+                if (count != null) {
+                    switch (count) {
+                        case 1:
+                            imageView.setImageResource(R.drawable.seeds);
+                            textView.setText("씨앗을 심은 상태입니다!\n 작은 새싹을 위해 일기를 작성해주세요.");
+                            break;
+                        case 2:
+                            imageView.setImageResource(R.drawable.small_sprout);
+                            textView.setText("작은 새싹이 자란 상태입니다!\n 큰 새싹을 위해 일기를 작성해주세요.");
+                            break;
+                        case 3:
+                            imageView.setImageResource(R.drawable.big_sprout);
+                            textView.setText("큰 새싹이 자란 상태입니다!\n 새로운 꽃을 위해 일기를 작성해주세요.");
+                            break;
+                        default:
+                            imageView.setImageResource(0); // 기본 이미지 또는 빈 이미지 설정
+                            textView.setText("아무것도 심어지지 않은 상태입니다!\n 일기를 작성하여 씨앗을 심어주세요.");
+                            break;
+                    }
                 }
             }
         });
-
         return view;
-    }
-
-
-    public class Adapter extends BaseAdapter {
-        Context context;
-        private List<Integer> flowerImages;
-
-        public Adapter(Context c) {
-            context = c;
-            flowerImages = new ArrayList<>();
-
-        }
-        public void setFlowerImages(List<Integer> flowerImages) {
-            this.flowerImages = flowerImages;
-            notifyDataSetChanged();
-        }
-        public int getCount() { return flowerImages.size(); } //그리드뷰에 보일 이미지 개수 반환
-        public Object getItem(int position) {
-            return flowerImages.get(position);
-        }
-        public long getItemId(int position) {
-            return position;
-        }
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView imageView;
-
-            if (convertView==null){
-                imageView = new ImageView(context);
-                imageView.setLayoutParams(new ViewGroup.LayoutParams(300,300));
-                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                imageView.setPadding(5,5,5,5);
-            }else{
-                imageView = (ImageView) convertView;
-            }
-            int flowerImage = flowerImages.get(position);
-            imageView.setImageResource(flowerImage);
-            imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    View dialogView = View.inflate(context, R.layout.dialog_f2, null);
-                    AlertDialog.Builder dlg = new AlertDialog.Builder(context);
-                    ImageView flowerInfo = dialogView.findViewById(R.id.flowerInfo);
-                    TextView flowerName = dialogView.findViewById(R.id.flowerName);
-                    TextView flowerMessage = dialogView.findViewById(R.id.flowerMessage);
-
-                    List<String> names = flowerViewModel.getFlowerNames().getValue();
-                    List<String> messages = flowerViewModel.getFlowerMessages().getValue();
-
-                    if (names != null && messages != null) {
-                        int flowerPosition = flowerImages.indexOf(flowerImage);
-                        flowerInfo.setImageResource(flowerImage);
-                        flowerName.setText(names.get(flowerPosition));
-                        flowerMessage.setText(messages.get(flowerPosition));
-                    }
-
-                    dlg.setView(dialogView);
-                    dlg.setNegativeButton("닫기", null);
-                    dlg.show();
-                }
-            });
-
-            return imageView;
-
-        }
-    }
-}
+    }}
